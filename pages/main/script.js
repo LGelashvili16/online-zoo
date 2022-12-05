8233e5b31dd9be9909148ea82d0b092d7b3fcf6d
LGellson <ladogelashvili16@gmail.com>
2022-12-06 03:02:37 +0400

    feat: add slider

--- pages/main/script.js
+++ pages/main/script.js
@@ -1,3 +1,5 @@
+'use strict'
+
 // ^^^^^^^^^^ Burger menu ^^^^^^^^^^
 
 const overlay = document.querySelector('.overlay');
@@ -28,3 +30,142 @@ header.addEventListener('click', function (e) {
 });
 
 // -----------++++++++++-----------++++++++++-----------++++++++++
+
+// ^^^^^^^^^^ Testimonials slider ^^^^^^^^^^
+
+const inputRange = document.querySelector('.input-range');
+const cards = document.querySelector('.s5-cards');
+
+inputRange.addEventListener('input', sliding);
+
+function sliding() {
+  let margin = `-${inputRange.value * 297}px`;
+  cards.style.marginLeft = margin;
+}
+
+
+// ^^^^^^^^^^ Testimonials popup ^^^^^^^^^^
+
+const cardTestimonials = document.querySelectorAll(".s5-card");
+const popup = document.querySelector('.popup');
+const popupInnerCard = document.querySelector('.popup-inner-card');
+const xIcon = document.querySelector('.popup-x-icon');
+const popupCard = document.querySelector('.popup-card');
+
+
+for (let i = 0; i < cardTestimonials.length; i++) {
+
+  let appendCard = cardTestimonials[i];
+  
+  cardTestimonials[i].addEventListener('click', () => {
+    popup.classList.add('popup-active');
+    popupInnerCard.innerHTML = appendCard.outerHTML;
+  });
+
+  xIcon.addEventListener('click', () => {
+    popup.classList.remove('popup-active');
+  });
+
+  popup.addEventListener('click', () => {
+    popup.classList.remove('popup-active');
+  });
+
+  popupCard.addEventListener('click', (e) => {
+    e.stopPropagation();
+  })
+
+};
+
+// -----------++++++++++-----------++++++++++-----------++++++++++
+
+// ^^^^^^^^^^ Pets slider ^^^^^^^^^^
+
+const leftArr = document.querySelector('.circle-left');
+const rightArr = document.querySelector('.circle-right');
+
+const petsSlider = document.getElementById('s3-cards-slider');
+const tableWidth = document.querySelector('.cards').clientWidth;
+const tables = document.querySelectorAll('.cards');
+
+let counter = 1;
+
+petsSlider.style.transform = `translateX(${-tableWidth * counter}px)`;
+
+
+rightArr.addEventListener('click', () => {
+
+  if (counter >= tables.length - 1) return;
+  petsSlider.style.transition = 'transform 0.5s';
+  counter++;
+  petsSlider.style.transform = `translateX(${-tableWidth * counter}px)`;
+
+});
+
+leftArr.addEventListener('click', () => {
+
+  if (counter <= 0) return;
+  petsSlider.style.transition = 'transform 0.5s';
+  counter--;
+  petsSlider.style.transform = `translateX(${-tableWidth * counter}px)`;
+
+});
+
+petsSlider.addEventListener('transitionend', () => {
+  if (tables[counter].id === 'last-clone') {
+    petsSlider.style.transition = 'none';
+    counter = tables.length - 2;
+    petsSlider.style.transform = `translateX(${-tableWidth * counter}px)`;
+  }
+
+  if (tables[counter].id === 'first-clone') {
+    petsSlider.style.transition = 'none';
+    counter = tables.length - counter;
+    petsSlider.style.transform = `translateX(${-tableWidth * counter}px)`;
+  }
+})
+
+// -----------++++++++++-----------++++++++++-----------++++++++++
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// leftArr.addEventListener('click', () => {
+//   counter++;
+//   petsSlider.style.transform = `translateX(${tableWidth * counter}px)`;
+  
+// })
+
+// rightArr.addEventListener('click', () => {
+  
+//   counter--;
+//   petsSlider.style.transform = `translateX(${tableWidth * counter}px)`;
+
+//   petsSlider.style.transition =  '1s';
+
+//   if (counter < 0) {
+//     console.log(counter);
+//   }
+
+//   petsSlider.addEventListener('transitionend', () => {
+//     if (tableWidth * counter < tableWidth + 10) {
+//       petsSlider.style.transition = 'none';
+//       petsSlider.style.transform = `translateX(-${tableWidth * counter}px)`;
+//     }
+//   })
+  
+// })
+
+
+
